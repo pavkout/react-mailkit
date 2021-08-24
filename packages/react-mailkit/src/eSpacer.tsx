913db63b1f52a0331fdd436fdbce7ec1976e2b10
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-08-24 08:58:47 +0300

    Added eSpacer component and test

---
+++ packages/react-mailkit/src/eSpacer.tsx
@@ -0,0 +1,24 @@
+import { createElement, FC } from 'react';
+
+import { IMJClass, IPaddingProps } from './propTypes/globalProps';
+import { px } from './propTypes/lengthUnit';
+import { normalizeProps } from './normalizers';
+
+export type ISpacerProps = {
+	/**
+	 * Inner element background color
+	 */
+	containerBackgroundColor?: string;
+	/**
+	 * Class name, added to the root HTML element created
+	 */
+	cssClass?: string;
+	/**
+	 * Spacer height
+	 */
+	height?: px;
+} & IPaddingProps &
+	IMJClass;
+
+export const ESpacer: FC<ISpacerProps> = (props) =>
+	createElement('mj-spacer', normalizeProps(props), null);
