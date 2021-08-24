326a5a9ecf4846cb6cc4208fac84a10f110660b1
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-08-24 09:08:18 +0300

    Added eTracking component and test

---
+++ packages/react-mailkit/src/eTracking.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { EImage } from './eImage';
+
+export type ITrackingProps = {
+	/**
+	 * The tracking url
+	 */
+	url: string;
+};
+
+export const ETracking = (props: ITrackingProps) => {
+	const { url } = props;
+
+	if (!url) {
+		throw new Error('You must specify a "url" attribute for Tracking');
+	}
+
+	return <EImage src={url} border="none" padding="0" width={1} height={1} />;
+};
