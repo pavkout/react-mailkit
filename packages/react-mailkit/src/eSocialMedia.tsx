0996aa99605183b5e4d15d9db4e901a2d37a98df
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-08-24 09:07:16 +0300

    Added eSocialMedia component

---
+++ packages/react-mailkit/src/eSocialMedia.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import map from 'lodash.map';
+
+import { ESocial, ISocialProps } from './eSocial';
+import { ESocialElement, ISocialElementProps } from './eSocialElement';
+
+export interface IMedia {
+  name: string;
+  href: string;
+}
+
+export type ISocialMediaProps = {
+  media: IMedia[];
+  socialProps?: ISocialProps;
+  socialElementProps?: ISocialElementProps;
+};
+
+export const ESocialMedia = (props: ISocialMediaProps) => {
+  const { media, socialProps, socialElementProps } = props;
+
+  if (!media || media.length === 0) {
+    throw new Error('You must specify a "media" attribute for SocialMedia');
+  }
+
+  return (
+    <ESocial {...socialProps}>
+      {map(media, (m: IMedia) => (
+        <ESocialElement
+          key={m.name}
+          name={m.name}
+          href={m.href}
+          {...socialElementProps}
+        >
+          {m.name}
+        </ESocialElement>
+      ))}
+    </ESocial>
+  );
+};
