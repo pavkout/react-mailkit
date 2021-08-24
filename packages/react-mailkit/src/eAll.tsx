d2afcaaf82adc4b1c84c67eb28d9224da9ec6413
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-08-24 08:38:44 +0300

    Added eAll component

---
+++ packages/react-mailkit/src/eAll.tsx
@@ -0,0 +1,9 @@
+import { createElement, FC } from 'react';
+
+import { normalizeProps } from './normalizers';
+import { IAllChildProps } from './propTypes/allChildProps';
+
+export type IAllProps = IAllChildProps;
+
+export const EAll: FC<IAllProps> = (props) =>
+	createElement('mj-all', normalizeProps(props), null);
