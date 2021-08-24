8c3b44ed65a19bad7bf69b9fecddcdec5815ae82
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-08-24 08:44:06 +0300

    Added eFont component and test

---
+++ packages/react-mailkit/src/eFont.tsx
@@ -0,0 +1,17 @@
+import { createElement, FC } from 'react';
+
+import { normalizeProps } from './normalizers';
+
+export type IFontProps = {
+	/**
+	 * URL of a hosted CSS file.
+	 */
+	href: string;
+	/**
+	 * Name of the font
+	 */
+	name: string;
+};
+
+export const EFont: FC<IFontProps> = (props) =>
+	createElement('mj-font', normalizeProps(props), null);
