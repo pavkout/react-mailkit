5c1114ee7d7b8b7dda95283d3b57d6d3366ae91c
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-08-24 08:38:13 +0300

    Added accordion component

---
+++ packages/react-mailkit/src/eAccordionText.tsx
@@ -0,0 +1,51 @@
+import { createElement, FC, ReactElement } from 'react';
+
+import { IMJClass, IPaddingProps } from './propTypes/globalProps';
+import { px } from './propTypes/lengthUnit';
+import { normalizeProps } from './normalizers';
+
+export type IAccordionTextProps = {
+	/**
+	 * Background color
+	 */
+	backgroundColor?: string;
+	/**
+	 * Text color
+	 */
+	color?: string;
+	/**
+	 * Class name, added to the root HTML element created
+	 */
+	cssClass?: string;
+	/**
+	 * Font family
+	 */
+	fontFamily?: string;
+	/**
+	 * Font Size
+	 */
+	fontSize?: number | string;
+	/**
+	 * Text thickness
+	 */
+	fontWeight?: number | string;
+	/**
+	 * Letter spacing px, em
+	 */
+	letterSpacing?: px;
+	/**
+	 * Space between the lines
+	 */
+	lineHeight?: px;
+	/**
+	 * Children
+	 */
+	children: string | ReactElement;
+} & IPaddingProps &
+	IMJClass;
+
+export const EAccordionText: FC<IAccordionTextProps> = (props) => {
+	const { children, ...rest } = props;
+
+	return createElement('mj-accordion-text', normalizeProps(rest), children);
+};
