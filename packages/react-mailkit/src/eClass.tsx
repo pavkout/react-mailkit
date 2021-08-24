c442138dd45f70fc86534b5f85021d0767a4da90
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-08-24 08:41:12 +0300

    Added eClass component

---
+++ packages/react-mailkit/src/eClass.tsx
@@ -0,0 +1,11 @@
+import { createElement, FC } from 'react';
+
+import { IAllChildProps } from './propTypes/allChildProps';
+import { normalizeProps } from './normalizers';
+
+export type IClassProps = {
+	name: string;
+} & IAllChildProps;
+
+export const EClass: FC<IClassProps> = (props) =>
+	createElement('mj-class', normalizeProps(props), null);
