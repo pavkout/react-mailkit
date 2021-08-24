09b79f769dab541610402a3260fc87110d2bf9c0
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-08-24 08:47:37 +0300

    Added eHtmlAttributes component and test

---
+++ packages/react-mailkit/src/eHtmlAttribute.tsx
@@ -0,0 +1,20 @@
+import { createElement, FC } from 'react';
+
+import { normalizeProps } from './normalizers';
+
+export type IHtmlAttributeProps = {
+	/**
+	 * Attribute name
+	 */
+	name: string;
+	/**
+	 * Children
+	 */
+	children: string;
+};
+
+export const EHtmlAttribute: FC<IHtmlAttributeProps> = (props) => {
+	const { children, ...rest } = props;
+
+	return createElement('mj-html-attribute', normalizeProps(rest), children);
+};
