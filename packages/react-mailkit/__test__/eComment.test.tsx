8490dda1922dacca86415450013c1294282f3d0f
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-08-24 09:04:50 +0300

    Added eComment component and test

---
+++ packages/react-mailkit/__test__/eComment.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+import { EComment } from '../src/eComment';
+
+describe('should be correct output', () => {
+	it('should not be undefined', () => {
+		const { container } = render(<EComment>My Comment here</EComment>);
+		expect(container).not.toBeUndefined();
+	});
+
+	it('should render EComment', () => {
+		const { container } = render(<EComment>My Comment here</EComment>);
+		expect(container.querySelector('mj-raw')).toBeInTheDocument();
+	});
+
+	it('should have correct condition', () => {
+		const { container } = render(
+			<EComment condition="if gte mso 9">MSO conditionals</EComment>,
+		);
+		expect(container.querySelector('mj-raw')).toHaveTextContent(
+			'<!--[if gte mso 9]>MSO conditionals<![endif]-->',
+		);
+	});
+
+	it('should have correct condition', () => {
+		const { container } = render(
+			<EComment condition="if IE">MSO conditionals</EComment>,
+		);
+		expect(container.querySelector('mj-raw')).toHaveTextContent(
+			'<!--[if IE]>MSO conditionals<![endif]-->',
+		);
+	});
+
+	it('renders correctly', () => {
+		expect(
+			renderer
+				.create(<EComment condition="if IE">MSO conditionals</EComment>)
+				.toJSON(),
+		).toMatchSnapshot();
+	});
+});
