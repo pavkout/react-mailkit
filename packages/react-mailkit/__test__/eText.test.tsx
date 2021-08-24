68998a5989670c69c8bb6b747de831c28e702456
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-08-24 09:00:34 +0300

    Added eText component and test

---
+++ packages/react-mailkit/__test__/eText.test.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+import { EText } from '../src/eText';
+
+const Text = (
+	<EText align="center" fontSize={20}>
+		I'm awesome email
+	</EText>
+);
+
+describe('should be correct output', () => {
+	it('should not be undefined', () => {
+		const { container } = render(Text);
+		expect(container).not.toBeUndefined();
+	});
+
+	it('should render EText', () => {
+		const { container } = render(Text);
+		expect(container.querySelector('mj-text')).toBeInTheDocument();
+	});
+
+	it('should have font-size attribute', () => {
+		const { container } = render(Text);
+		expect(
+			container.querySelector('mj-text')?.hasAttribute('font-size'),
+		).toBeTruthy();
+	});
+
+	it('should have 20px as font-size attribute', () => {
+		const { container } = render(Text);
+		expect(
+			container.querySelector('mj-text')?.getAttribute('font-size'),
+		).toStrictEqual('20px');
+	});
+
+	it('should have align attribute', () => {
+		const { container } = render(Text);
+		expect(
+			container.querySelector('mj-text')?.hasAttribute('align'),
+		).toBeTruthy();
+	});
+
+	it('should have center as align attribute', () => {
+		const { container } = render(Text);
+		expect(
+			container.querySelector('mj-text')?.getAttribute('align'),
+		).toStrictEqual('center');
+	});
+
+	it('should have correct content', () => {
+		const { container } = render(Text);
+		expect(container.querySelector('mj-text')).toHaveTextContent(
+			`I'm awesome email`,
+		);
+	});
+
+	it('renders correctly', () => {
+		expect(renderer.create(Text).toJSON()).toMatchSnapshot();
+	});
+});
---
+++ packages/react-mailkit/src/eText.tsx
@@ -0,0 +1,78 @@
+import { createElement, FC, ReactElement } from 'react';
+
+import {
+	IPaddingProps,
+	IAlign,
+	IFontStyle,
+	ITextDecoration,
+	ITextTransform,
+	IMJClass,
+} from './propTypes/globalProps';
+import { px } from './propTypes/lengthUnit';
+import { normalizeProps } from './normalizers';
+
+export type ITextProps = {
+	/**
+	 * Text color
+	 */
+	color?: string;
+	/**
+	 * Font name
+	 */
+	fontFamily?: string;
+	/**
+	 * Font size
+	 */
+	fontSize?: px;
+	/**
+	 * Font style
+	 */
+	fontStyle?: IFontStyle;
+	/**
+	 * Text thickness
+	 */
+	fontWeight?: number | string;
+	/**
+	 * Space between the lines
+	 */
+	lineHeight?: px;
+	/**
+	 * Letter spacing (px,em)
+	 */
+	letterSpacing?: px;
+	/**
+	 * The height of the element
+	 */
+	height?: px;
+	/**
+	 * underline/overline/line-through/none
+	 */
+	textDecoration?: ITextDecoration;
+	/**
+	 * uppercase/lowercase/capitalize
+	 */
+	textTransform?: ITextTransform;
+	/**
+	 * left/right/center/justify
+	 */
+	align?: IAlign | 'justify';
+	/**
+	 * Inner element background color
+	 */
+	containerBackgroundColor?: string;
+	/**
+	 * Class name, added to the root HTML element created
+	 */
+	cssClass?: string;
+	/**
+	 * Children
+	 */
+	children?: string | ReactElement | ReactElement[] | any[];
+} & IPaddingProps &
+	IMJClass;
+
+export const EText: FC<ITextProps> = (props) => {
+	const { children, ...rest } = props;
+
+	return createElement('mj-text', normalizeProps(rest), children);
+};
