b5225a02023b0f58e9568b27d29cc88871eb7f93
Pavlos Koutoglou <pkoutoglou@ordereze.com>
2021-09-01 22:48:54 +0300

    Added storybook configuration

---
+++ packages/react-mailkit/.storybook/theme.js
@@ -0,0 +1,16 @@
+import { create } from '@storybook/theming';
+import logo from './logo.png';
+
+export const theme = create({
+  base: 'dark',
+  appBorderRadius: 3,
+  colorPrimary: '#86c232',
+  colorSecondary: '#86c232',
+  barSelectedColor: '#86c232',
+  barTextColor: '#86c232',
+  barSelectedColor: '#86c232',
+  gridCellSize: 12,
+  brandTitle: 'react-mailkit storybook',
+  brandUrl: 'https://react-mailkit.io',
+  brandImage: logo
+});
